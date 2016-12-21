22e1a447f573a79df97606b59b72b4c0c8c0d037
Philippe Correia <philippe.barbosa@ufba.br>
2016-12-21 10:49:44 -0300

    Home com Chartist

---
+++ App/views/home_chartist.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <?php include('../includes/HEAD.INC'); ?>
+  <body>
+    <?php include('../includes/header.php'); ?>
+    <script src="https://github.com/gionkunz/chartist-js/blob/develop/dist/chartist.min.js"></script>
+  </body>
+</html>
