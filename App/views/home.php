244fa9d642b5858e865cbad994e94470b72bc450
Philippe Correia <philippe.barbosa@ufba.br>
2017-01-11 18:18:53 -0300

    Gambiarra de tabela usada pra captar valores. Arranjar maneira melhor de fazer. Além disso, organizar as datas, pois são strings e estão fora de ordem.

--- App/views/home.php
+++ App/views/home.php
@@ -16,25 +16,65 @@
         $conn = Database::closeConn();
         $_SESSION['pesagens'] = $pesagens;
         //Utils::debugarVariavel($_SESSION);
+
       ?>
     </div>
+    <table id="tabela" hidden>
+      <thead>
+        <td>data</td>
+        <td>peso</td>
+      </thead>
+      <tbody>
+        <?php
+          $tam = count($pesagens);
+          for ($i=0; $i < $tam; $i++) {
+            echo '<tr>
+                    <td id="data'.$i.'" class="datas-tabela">'.$pesagens[$i]['ps_data'].'</td>
+                    <td id="peso'.$i.'" class="pesos-tabela">'.$pesagens[$i]['ps_peso'].'</td>
+                  </tr>';
+          }
+        ?>
+      </tbody>
+    </table>
     <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
     <script type="text/javascript">
+      var tabela = document.getElementById('tabela');
+      var datas = document.getElementsByClassName('datas-tabela');
+      var pesos = document.getElementsByClassName('pesos-tabela');
+      var datas_valores = [];
+      var pesos_valores = [];
+
+      for (var i = 0; i < datas.length; i++) {
+        datas_valores.push(datas[i].innerHTML);
+        pesos_valores.push(parseFloat(pesos[i].innerHTML));
+      }
+
+      var min = Math.min.apply(null, pesos_valores);
+      var max = Math.max.apply(null, pesos_valores);
+
       var chartData = {
         type:"line",
         title: {
           text: "Pesagem"
         },
+        "scale-x": {
+          "labels": datas_valores
+        },
+        "scale-y": {
+          "min-value": min,
+          "max-value": max,
+          "step": 1
+        },
         // legend: {},
         series: [ // valores aqui
-          {values: [1, 4, 3, 5, 2, 6]} // fazer esse array receber direto da session
+          {values: pesos_valores} // fazer esse array receber direto da session
         ]
       };
       zingchart.render({
         id: "chartDiv",
         data: "chartData",
         height: 400,
-        width: 600
+        width: 1200
       });
     </script>
   </body>
